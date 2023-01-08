a400ad42d85ae5b8a264a049532039c818b34c7b
Dori <jaewan0114@naver.com>
2023-01-08 16:04:55 +0900

    splash demo

---
+++ app/src/main/java/com/example/umc_pj/SplashActivity.kt
@@ -0,0 +1,46 @@
+package com.example.umc_pj
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umc_pj.databinding.SplashBinding
+
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: SplashBinding
+
+    lateinit var FadeIn : Animation
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SplashBinding.inflate(layoutInflater)
+        setContentView(binding.root);
+
+        FadeIn = AnimationUtils.loadAnimation(this,R.anim.fadein_splash)
+        FadeIn.setAnimationListener(SlidingPageAnimationListener())
+        binding.logo.startAnimation(FadeIn)
+        // 일정 시간 지연 이후 실행하기 위한 코드
+        Handler(Looper.getMainLooper()).postDelayed({
+            // 일정 시간이 지나면 MainActivity로 이동
+            val intent= Intent( this,UserAgreeActivity::class.java)
+            startActivity(intent)
+            // 이전 키를 눌렀을 때 스플래스 스크린 화면으로 이동을 방지하기 위해
+            // 이동한 다음 사용안함으로 finish 처리
+            finish()
+
+        }, 2000) // 시간 2초 이후 실행
+
+    }
+
+    private inner class SlidingPageAnimationListener : Animation.AnimationListener {
+
+        override fun onAnimationEnd(animation: Animation?) : Unit {
+        }
+        override fun onAnimationStart(animation: Animation?) {}
+        override fun onAnimationRepeat(animation: Animation?) {}
+    }
+}
