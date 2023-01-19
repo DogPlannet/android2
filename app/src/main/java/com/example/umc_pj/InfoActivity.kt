aa3e0667e57a6aa6866eaa635ebb8ff7a1e9562a
jiohjung98 <ckaquz98@naver.com>
2023-01-20 03:45:34 +0900

    바텀네비게이션뷰&회원가입 버튼 활성화 임의로 작성

--- app/src/main/java/com/example/umc_pj/InfoActivity.kt
+++ app/src/main/java/com/example/umc_pj/InfoActivity.kt
@@ -5,16 +5,29 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.umc_pj.databinding.ActivityInfoBinding
+import kotlinx.android.synthetic.main.activity_dog_register.*
 import kotlinx.android.synthetic.main.activity_info.*
+import java.util.regex.Pattern
 
 
 class InfoActivity: AppCompatActivity() {
+
+    // 에딧텍스트에 값 4개 다 들어가야 버튼 활성화되는 코드
+    var validEditText1: Boolean= false
+    var validEditText2: Boolean= false
+    var validEditText3: Boolean= false
+    var validEditText4: Boolean= false
 
     private lateinit var binding: ActivityInfoBinding
 
@@ -22,6 +35,60 @@ class InfoActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 에딧텍스트에 값 4개 다 들어가야 버튼 활성화되는 코드
+        binding.etName.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable) {
+                validEditText1 = editable.isNotEmpty()
+                checkValid(validEditText1, validEditText2, validEditText3, validEditText4)
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                p0?.let { highlightText(it as Editable) }
+            }
+        })
+
+        binding.editTextTextEmailAddress.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable) {
+                validEditText2 = editable.isNotEmpty()
+                checkValid(validEditText1, validEditText2, validEditText3, validEditText4)
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+
+        binding.editTextTextPassword1.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable) {
+                validEditText3 = editable.isNotEmpty()
+                checkValid(validEditText1, validEditText2, validEditText3, validEditText4)
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                p0?.let { highlightText(it as Editable) }
+            }
+        })
+
+        binding.editTextTextPassword2.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable) {
+                validEditText4 = editable.isNotEmpty()
+                checkValid(validEditText1, validEditText2, validEditText3, validEditText4)
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                p0?.let { highlightText(it as Editable) }
+            }
+        })
+
+
 
 //        var name = binding.etName.text.toString()
 //        var email = binding.editTextTextEmailAddress.text.toString()
@@ -43,5 +110,18 @@ class InfoActivity: AppCompatActivity() {
         val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         return true
+    }
+//    에딧텍스트 활성화 코드
+    private fun checkValid(v1:Boolean, v2:Boolean, v3:Boolean, v4:Boolean){
+        Log.d("Valid", (v1 && v2 && v3 && v4).toString())
+        if(v1 && v2 && v3 && v4){
+            next_btn.isEnabled = true
+            next_btn.isClickable = true
+            next_btn.setBackgroundResource(R.drawable.start_button)
+        } else {
+            next_btn.isEnabled = false
+            next_btn.isClickable = false
+            next_btn.setBackgroundResource(R.drawable.disabled_button)
+        }
     }
 }
