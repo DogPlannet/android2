8f3ab18e5ec49e7c73776c36e1f2084819f33b58
Yoonjit <jisu9083@naver.com>
2023-01-24 21:04:08 +0900

    회원가입 오류 수정 및 버튼 활성화

--- app/src/main/java/com/example/umc_pj/InfoActivity.kt
+++ app/src/main/java/com/example/umc_pj/InfoActivity.kt
@@ -2,21 +2,25 @@ package com.example.umc_pj
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.umc_pj.databinding.ActivityInfoBinding
+import kotlinx.android.synthetic.main.activity_info.*
 import java.util.regex.Pattern
 
 class InfoActivity: AppCompatActivity() {
 
     // 에딧텍스트에 값 4개 다 들어가야 버튼 활성화되는 코드
-//    var validEditText1: Boolean= false
-//    var validEditText2: Boolean= false
-//    var validEditText3: Boolean= false
-//    var validEditText4: Boolean= false
+    var validEditText1: Boolean = false
+    var validEditText2: Boolean = false
+    var validEditText3: Boolean = false
+    var validEditText4: Boolean = false
 
     private lateinit var binding: ActivityInfoBinding
 
@@ -25,103 +29,133 @@ class InfoActivity: AppCompatActivity() {
         binding = ActivityInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.etName.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+
+            override fun afterTextChanged(p0: Editable?) {
+                if(et_name.length() > 0){
+                    validEditText1 = true
+                    if (validEditText1 && validEditText2 && validEditText3 && validEditText4){
+                        next_btn.isClickable = true
+                        next_btn.isEnabled = true
+                    }
+                } else{
+                    validEditText1 = false
+                    next_btn.isClickable = false
+                    next_btn.isEnabled = false
+                }
+            }
+        })
+
+        binding.editTextTextEmailAddress.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+
+            override fun afterTextChanged(p0: Editable?) {
+                if(editTextTextEmailAddress.length() > 0){
+                    validEditText2 = true
+                    if (validEditText1 && validEditText2 && validEditText3 && validEditText4){
+                        next_btn.isClickable = true
+                        next_btn.isEnabled = true
+                    }
+                } else{
+                    validEditText2 = false
+                    next_btn.isClickable = false
+                    next_btn.isEnabled = false
+                }
+            }
+        })
+
+        binding.editTextTextPassword1.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+
+            override fun afterTextChanged(p0: Editable?) {
+                if(editTextTextPassword1.length() > 0){
+                    validEditText3 = true
+                    if (validEditText1 && validEditText2 && validEditText3 && validEditText4){
+                        next_btn.isClickable = true
+                        next_btn.isEnabled = true
+                    }
+                } else{
+                    validEditText3 = false
+                    next_btn.isClickable = false
+                    next_btn.isEnabled = false
+                }
+            }
+        })
+
+        binding.editTextTextPassword2.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+
+            override fun afterTextChanged(p0: Editable?) {
+                if(editTextTextPassword2.length() > 0){
+                    validEditText4 = true
+                    if (validEditText1 && validEditText2 && validEditText3 && validEditText4){
+                        next_btn.isClickable = true
+                        next_btn.isEnabled = true
+                    }
+                } else{
+                    validEditText4 = false
+                    next_btn.isClickable = false
+                    next_btn.isEnabled = false
+                }
+            }
+        })
+
+
+
             binding.nextBtn.setOnClickListener {
+                var name = binding.etName.text.toString() // 이름
                 var email = binding.editTextTextEmailAddress.text.toString() // 이메일
                 var pw1 = binding.editTextTextPassword1.text.toString() // 비밀번호
                 var pw2 = binding.editTextTextPassword2.text.toString() // 비밀번호 확인
 
                 if(email.isEmpty()){
                     binding.emailLayout.visibility = View.VISIBLE
+                    next_btn.isEnabled = false
                 } else if (!email.contains("@")){
                     binding.emailLayout.visibility = View.VISIBLE
                 }
 
                 if(pw1.isEmpty()){
                     binding.pw1Layout.visibility = View.VISIBLE
+                    next_btn.isEnabled = false
                 } else if (!Pattern.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\$!%*#?&])[A-Za-z\\d@\$!%*#?&]{8,}\$", pw1)) {
                     binding.pw1Layout.visibility = View.VISIBLE
                 }
 
-
                 if(pw2.isEmpty()){
                     binding.pw2Layout.visibility = View.VISIBLE
+                    next_btn.isEnabled = false
                 } else if (pw1 != pw2){
                     binding.pw2Layout.visibility = View.VISIBLE
                 }
 
-                else{
+                if(Pattern.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\$!%*#?&])[A-Za-z\\d@\$!%*#?&]{8,}\$", pw1) && (pw1 == pw2) && (email.contains("@")) && (name.isNotEmpty())){
                     val intent = Intent(this, UserAgreeActivity::class.java)
                     startActivity(intent)
                 }
             }
-//        // 에딧텍스트에 값 4개 다 들어가야 버튼 활성화되는 코드
-//        binding.etName.addTextChangedListener(object : TextWatcher {
-//            override fun afterTextChanged(editable: Editable) {
-//                validEditText1 = editable.isNotEmpty()
-//                checkValid(validEditText1, validEditText2, validEditText3, validEditText4)
-//            }
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            }
-//
-//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-////                p0?.let { highlightText(it as Editable) }
-//            }
-//        })
 
-//        binding.editTextTextEmailAddress.addTextChangedListener(object : TextWatcher {
-//            override fun afterTextChanged(editable: Editable) {
-//                validEditText2 = editable.isNotEmpty()
-//                checkValid(validEditText1, validEditText2, validEditText3, validEditText4)
-//            }
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            }
-//
-//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            }
-//        })
-//
-//        binding.editTextTextPassword1.addTextChangedListener(object : TextWatcher {
-//            override fun afterTextChanged(editable: Editable) {
-//                validEditText3 = editable.isNotEmpty()
-//                checkValid(validEditText1, validEditText2, validEditText3, validEditText4)
-//            }
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            }
-//
-//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-////                p0?.let { highlightText(it as Editable) }
-//            }
-//        })
-//
-//        binding.editTextTextPassword2.addTextChangedListener(object : TextWatcher {
-//            override fun afterTextChanged(editable: Editable) {
-//                validEditText4 = editable.isNotEmpty()
-//                checkValid(validEditText1, validEditText2, validEditText3, validEditText4)
-//            }
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            }
-//
-//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-////                p0?.let { highlightText(it as Editable) }
-//            }
-//        })
-
-
-
-//        var name = binding.etName.text.toString()
-//        var email = binding.editTextTextEmailAddress.text.toString()
-//        var password1 = binding.editTextTextPassword1.text.toString()
-//        var password2 = binding.editTextTextPassword2.text.toString()
-
-//        if (name != "" && email != "" && password1 != "" && password2 != ""){
-//            binding.nextBtn.setBackgroundColor(Color.parseColor("#574444"))
-//            binding.nextBtn.setTextColor(Color.parseColor("#e2e2e2"))
-
-//            binding.nextBtn.setOnClickListener {
-//                val intent = Intent(this, UserAgreeActivity::class.java)
-//                startActivity(intent)
-//            }
-//        }
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -133,17 +167,4 @@ class InfoActivity: AppCompatActivity() {
         return true
     }
 
-//    에딧텍스트 활성화 코드
-//    private fun checkValid(v1:Boolean, v2:Boolean, v3:Boolean, v4:Boolean){
-//        Log.d("Valid", (v1 && v2 && v3 && v4).toString())
-//        if(v1 && v2 && v3 && v4){
-//            next_btn.isEnabled = true
-//            next_btn.isClickable = true
-//            next_btn.setBackgroundResource(R.drawable.start_button)
-//        } else {
-//            next_btn.isEnabled = false
-//            next_btn.isClickable = false
-//            next_btn.setBackgroundResource(R.drawable.disabled_button)
-//        }
-//    }
 }
