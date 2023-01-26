00b195a2cee3566d6ce97a96c2254cf43d0f06f5
jiohjung98 <ckaquz98@naver.com>
2023-01-26 18:14:03 +0900

    비밀번호 찾기 레이아웃

---
+++ app/src/main/java/com/example/umc_pj/FindPwActivity.kt
@@ -0,0 +1,11 @@
+package com.example.umc_pj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindPwActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_pw)
+    }
+}
