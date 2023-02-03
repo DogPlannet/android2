1d966f2ef023614f78b043692a977e44802690dd
JWdori <jaewan0114@naver.com>
2023-02-04 03:13:31 +0900

    홈 나머지 다이얼로그 디자인

--- app/src/main/java/com/example/umc_pj/homepackage/MyDialog.kt
+++ app/src/main/java/com/example/umc_pj/homepackage/MyDialog.kt
@@ -1,39 +1,42 @@
 package com.example.umc_pj.homepackage
 
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.util.Log
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.umc_pj.databinding.CustomdialogBinding
+import com.example.umc_pj.databinding.CustomdialogWalkFailBinding
+import com.example.umc_pj.databinding.CustomdialogWalkStartBinding
 
 class MyDialog(private val context : AppCompatActivity) {
     private lateinit var listener : MyDialogOKClickedListener
     private lateinit var binding : CustomdialogBinding
+    private lateinit var binding2 : CustomdialogWalkStartBinding
+    private lateinit var binding3 : CustomdialogWalkFailBinding
     private val dlg = Dialog(context)   //부모 액티비티의 context 가 들어감
 
-    fun show(content : String) {
+    fun show() {
         binding = CustomdialogBinding.inflate(context.layoutInflater)
-
         dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
         dlg.setContentView(binding.root)     //다이얼로그에 사용할 xml 파일을 불러옴
         dlg.setCancelable(true)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
-
+        dlg.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         val params: WindowManager.LayoutParams = this.dlg.window!!.attributes
-        params.y = 900
+        params.y = 800
         this.dlg.window!!.attributes = params
-
-//        //ok 버튼 동작
-//        binding.ok.setOnClickListener {
-//
-//            //TODO: 부모 액티비티로 내용을 돌려주기 위해 작성할 코드
-//
-//            dlg.dismiss()
-//        }
-//
-//        //cancel 버튼 동작
-//        binding.cancel.setOnClickListener {
-//            dlg.dismiss()
-//        }
+        dlg.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        //ok 버튼 동작
+        binding.yes.setOnClickListener {
+            dlg.dismiss()
+            walkstart()
+        }
+        //cancel 버튼 동작
+        binding.no.setOnClickListener {
+            dlg.dismiss()
+        }
 
         dlg.show()
     }
@@ -45,6 +48,36 @@ class MyDialog(private val context : AppCompatActivity) {
             }
         }
     }
+    fun walkstart(){
+        binding2 = CustomdialogWalkStartBinding.inflate(context.layoutInflater)
+//            dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
+        dlg.setContentView(binding2.root)     //다이얼로그에 사용할 xml 파일을 불러옴
+        dlg.setCancelable(true)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
+        dlg.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val params: WindowManager.LayoutParams = this.dlg.window!!.attributes
+        params.y = 700
+        this.dlg.window!!.attributes = params
+        dlg.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        dlg.show()
+    }
+
+    fun walkfail(){
+        binding3 = CustomdialogWalkFailBinding.inflate(context.layoutInflater)
+//            dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
+        dlg.setContentView(binding3.root)     //다이얼로그에 사용할 xml 파일을 불러옴
+        dlg.setCancelable(true)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
+        dlg.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val params: WindowManager.LayoutParams = this.dlg.window!!.attributes
+        params.y = 800
+        this.dlg.window!!.attributes = params
+        dlg.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        dlg.show()
+        binding3.walkResultClose.setOnClickListener {
+            dlg.dismiss()
+        }
+    }
+
+
 
     interface MyDialogOKClickedListener {
         fun onOKClicked(content : String)
