c8fede9898632aa8779ea6752d6df5e7ef087774
JWdori <jaewan0114@naver.com>
2023-02-04 20:04:42 +0900

    레이아웃 마무리

--- app/src/main/java/com/example/umc_pj/homepackage/MyDialog.kt
+++ app/src/main/java/com/example/umc_pj/homepackage/MyDialog.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.util.Log
+import android.view.Gravity
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,7 @@ import com.bumptech.glide.Glide
 import com.example.umc_pj.R
 import com.example.umc_pj.databinding.CustomdialogBinding
 import com.example.umc_pj.databinding.CustomdialogWalkFailBinding
+import com.example.umc_pj.databinding.CustomdialogWalkResultBinding
 import com.example.umc_pj.databinding.CustomdialogWalkStartBinding
 
 class MyDialog(private val context : AppCompatActivity) {
@@ -18,6 +20,7 @@ class MyDialog(private val context : AppCompatActivity) {
     private lateinit var binding : CustomdialogBinding
     private lateinit var binding2 : CustomdialogWalkStartBinding
     private lateinit var binding3 : CustomdialogWalkFailBinding
+    private lateinit var binding4 : CustomdialogWalkResultBinding
     private val dlg = Dialog(context)   //부모 액티비티의 context 가 들어감
 
     fun show() {
@@ -38,6 +41,8 @@ class MyDialog(private val context : AppCompatActivity) {
         //cancel 버튼 동작
         binding.no.setOnClickListener {
             dlg.dismiss()
+            walkresult()
+
         }
 
         dlg.show()
@@ -57,7 +62,7 @@ class MyDialog(private val context : AppCompatActivity) {
         Glide.with(context).load(R.drawable.movedog).override(200, 200).into(binding2.moveGif)
 //이러면 멈춤        Glide.with(context).load(R.drawable.walk_box).override(200, 200).into(binding2.moveGif)
 
-        dlg.setCancelable(true)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
+        dlg.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
         dlg.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         val params: WindowManager.LayoutParams = this.dlg.window!!.attributes
         params.y = 650
@@ -78,6 +83,20 @@ class MyDialog(private val context : AppCompatActivity) {
         dlg.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
         dlg.show()
         binding3.walkResultClose.setOnClickListener {
+            dlg.dismiss()
+        }
+    }
+    fun walkresult(){
+        binding4 = CustomdialogWalkResultBinding.inflate(context.layoutInflater)
+        dlg.setContentView(binding4.root)     //다이얼로그에 사용할 xml 파일을 불러옴
+        dlg.setCancelable(true)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
+        dlg.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val params: WindowManager.LayoutParams = this.dlg.window!!.attributes
+        params.y = 100
+        this.dlg.window!!.attributes = params
+        dlg.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        dlg.show()
+        binding4.walkResultClose.setOnClickListener {
             dlg.dismiss()
         }
     }
