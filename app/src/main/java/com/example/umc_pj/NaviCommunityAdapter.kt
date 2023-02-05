8ca1e941fb99446d6bcecc313191e9f0e7535539
Yoonjit <jisu9083@naver.com>
2023-02-05 23:18:35 +0900

    리사이클러뷰 아이템 클릭 이벤트 설정

--- app/src/main/java/com/example/umc_pj/NaviCommunityAdapter.kt
+++ app/src/main/java/com/example/umc_pj/NaviCommunityAdapter.kt
@@ -4,11 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class NaviCommunityAdapter(private val itemList: ArrayList<NaviCommunityModel>):
         RecyclerView.Adapter<NaviCommunityAdapter.NaviCommunityViewHolder>(){
-
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NaviCommunityViewHolder {
                 val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_community_rc_view_item, parent, false)
@@ -25,6 +27,18 @@ class NaviCommunityAdapter(private val itemList: ArrayList<NaviCommunityModel>):
                 holder.dog.text = itemList[position].dog
                 holder.heartcnt.text = itemList[position].heartcnt
                 holder.commentcnt.text = itemList[position].commentcnt
+
+                holder.itemView.setOnClickListener(object : View.OnClickListener{
+                        override fun onClick(p0: View?) {
+                                val activity=p0!!.context as AppCompatActivity
+                                val activity2 = p0!!.context as MainActivity
+                                val communityItemFragment = CommunityItemFragment()
+                                activity.supportFragmentManager.beginTransaction().replace(R.id.main_frm, communityItemFragment)
+                                        .addToBackStack(null).commit()
+                                activity.toolbar.visibility = View.INVISIBLE
+                                activity.toolbar3.visibility = View.VISIBLE
+                        }
+                })
         }
 
         class NaviCommunityViewHolder(itemView: View) :  RecyclerView.ViewHolder(itemView){
